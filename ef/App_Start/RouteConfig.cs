08aacb35e41695e7755d59e3845f826e56d3dccc
muhammadandikakurniawan <muhammadandikakurniawan24@gmail.com>
2019-07-10 11:24:10 +0700

    change route map

--- ef/App_Start/RouteConfig.cs
+++ ef/App_Start/RouteConfig.cs
@@ -13,6 +13,8 @@ namespace ef
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
+            routes.MapMvcAttributeRoutes();
+
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
