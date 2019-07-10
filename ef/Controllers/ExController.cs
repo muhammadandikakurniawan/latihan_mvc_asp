b01fd68cf389037e8c76ea4f2ee3db2cd3acc987
muhammadandikakurniawan <muhammadandikakurniawan24@gmail.com>
2019-07-10 11:32:55 +0700

    add excontroller

---
+++ ef/Controllers/ExController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ef.Controllers
+{
+    public class ExController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
