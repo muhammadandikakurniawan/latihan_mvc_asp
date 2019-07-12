f6dfa7939d048312f06962a81f8e758119e78832
muhammadandikakurniawan <muhammadandikakurniawan24@gmail.com>
2019-07-12 17:15:47 +0700

    save

--- Api/Controllers/HomeController.cs
+++ Api/Controllers/HomeController.cs
@@ -3,6 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Api.Models;
+using Api.Filters;
+using Api.DTO;
+using System.Net.Http;
+using System.Net.Http.Headers;
 
 namespace Api.Controllers
 {
@@ -13,6 +18,31 @@ namespace Api.Controllers
             ViewBag.Title = "Home Page";
 
             return View();
+        }
+
+        [HttpGet]
+        [Route("apiclient")]
+        public string ApiClient()
+        {
+            
+            HttpClient client = new HttpClient();
+
+            client.BaseAddress = new Uri("http://localhost:58762/");
+
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic", "cmluYTpzaWxmaWE=");
+
+            var get = client.GetAsync("getprincipal");
+            get.Wait();
+
+            var result = get.Result;
+
+            if (result.IsSuccessStatusCode)
+            {
+                var read = result.Content.ReadAsAsync<string>();
+                read.Wait();
+                return read.Result;
+            }
+            return "fail";
         }
     }
 }
